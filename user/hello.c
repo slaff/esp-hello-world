f9c0aa43292cf49883a42bc1f8d8831b07592849
Slavey Karadzhov <slaff@linux-bg.org>
2015-03-13 10:38:22 +0000

    First version of our hello world example code. tags:[init]

--- user/hello.c
+++ user/hello.c
@@ -1,6 +1,13 @@
-#include <stdio.h>
+#include "ets_sys.h"
+#include "osapi.h"
+#include "os_type.h"
+#include "user_config.h"
  
-int main() {
-    printf("Hello, world!\n");
-    return 0;
+void user_init()
+{
+    // This is used to setup the serial communication
+    uart_div_modify(0, UART_CLK_FREQ / 115200);
+
+    // And this is used to print some information
+    os_printf("Hello, world!\n");
 }
