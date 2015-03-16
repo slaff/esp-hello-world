7511475ed2a4cb23a25d4e42652f8c9c89181c14
Slavey Karadzhov <slavey.karadzhov@zend.com>
2015-03-16 16:41:59 +0100

    Start our code once the initialization is done. tags:[start]

--- user/hello.c
+++ user/hello.c
@@ -3,11 +3,20 @@
 #include "os_type.h"
 #include "user_config.h"
  
+void start(void)
+{
+	// And this is used to print some information once the device is initialized
+	os_printf("Hello, world!\n");
+}
+
+/*
+ * user_init should be used mainly for initialization
+ */
 void user_init()
 {
     // This is used to setup the serial communication
     uart_div_modify(0, UART_CLK_FREQ / 115200);
 
-    // And this is used to print some information
-    os_printf("Hello, world!\n");
+    // once the initialization is done we can start with our code
+    system_init_done_cb(start);
 }
